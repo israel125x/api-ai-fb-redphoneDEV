cb5e263a2ae908fe0e7b4f86c6e730a71922d937
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-04 13:47:37 -0500

    040817145
    040817145

--- src/app.js
+++ src/app.js
@@ -233,7 +233,6 @@ class FacebookBot {
     getEventText(event) {
         if (event.message) {
 			if(event.messageobj.type){
-			console.log('event.messageobj.type '+ JSON.stringify(event.messageobj.type);	
 			}
             if (event.message.quick_reply && event.message.quick_reply.payload) {
 				console.log('event.message.quick_reply.payload= '+ event.message.quick_reply.payload);
