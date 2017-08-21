834033c89f81ab7471fdf5733b48bbb5af4a2745
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-21 15:48:58 -0500

    ret343
    gwreegre324234

--- src/app.js
+++ src/app.js
@@ -44,19 +44,19 @@ function enviarEncuestasSol(){
   var db = firebase.database();
   var ref = db.ref("fbregistro/"); 
   //---------------------------------------------------
- var count = 0;
+  var count = 0;
 
-  ref.on("child_added", function(snap) {
+ref.on("child_added", function(snap) {
   count++;
   console.log("added:", snap.key);
 });
 
 // length will always equal count, since snap.val() will include every child_added event
 // triggered before this point
-  ref.once("value", function(snap) {
+ref.once("value", function(snap) {
   console.log("initial data loaded!", snap.numChildren() === count);
 });
-//ref.off("value", originalCallback, this);
+ 
 }
 function wfirebase (idusr, estado, respuesta){
 	 
