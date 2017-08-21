7b90b2c28c3cd9f9b8d8a3250a323eb69bada982
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-21 15:22:05 -0500

    dsfsdf
    sdfsdfdsf3e23r243r

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ var config = {
     storageBucket: "turnosmovil-a576d.appspot.com",
     messagingSenderId: "706329874359"
   };
+
 function enviarEncuestasSol(){
   console.log("conectando a FireBase");
   var defaultApp = firebase.initializeApp(config);
