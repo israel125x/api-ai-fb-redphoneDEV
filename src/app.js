17460c5d1d4bc4cbd3d5361466ca11ff9d8cf7a9
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-22 16:01:00 -0500

    ger45
    4543543rtretert

--- src/app.js
+++ src/app.js
@@ -581,7 +581,7 @@ app.post('/webhook/', (req, res) => {
 
                             if (event.message.attachments) {
 								console.log('event.message.attachments= ',event.message.attachments);
-								console.log('event.message.attachments.type= ',event.message.attachments[0].payload.url);
+								console.log('event.message.attachments[0].payload.url= ',event.message.attachments[0].payload.url);
 								
                                 let locations = event.message.attachments.filter(a => a.type === "location");
                                 // delete all locations from original message
