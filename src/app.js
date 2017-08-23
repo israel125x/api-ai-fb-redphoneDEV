e27c0c56542b4205dfe9fc5e9c1730002828b447
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-23 11:07:04 -0500

    dsfsdfdsf43242343
    ewrerwere3243434324

--- src/app.js
+++ src/app.js
@@ -316,14 +316,39 @@ class FacebookBot {
 		console.log('event.sender.id.toString= '+event.sender.id.toString());
 		console.log('event.sender.id.toString= '+event.message.attachments[0].payload.url.toString());
 		console.log('enviando URL');
-		//this.doTextResponse(event.sender.id.toString(),event.message.attachments[0].payload.url.toString());
+		/*this.doTextResponse(event.sender.id.toString(),event.message.attachments[0].payload.url.toString());
 			let messageData = {
-				"attachment": {
-					"type": "image",
-					"payload": { url: event.message.attachments[0].payload.url.toString()}
-				}
-			}
-		sendFBMessage (event.sender.id,messageData);	
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": event.message.attachments[0].payload.url,
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": event.message.attachments[0].payload.url,
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+		sendFBMessage (event.sender.id,messageData);	*/
 		console.log('return null');
         return null;
     }
