b537f0b0ccdcd55f9ba049ec69184ba60da6b421
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-23 10:39:39 -0500

    fdfd4343rfere
    erwerew34234324fdsfsdfdsf

--- src/app.js
+++ src/app.js
@@ -316,15 +316,14 @@ class FacebookBot {
 		console.log('event.sender.id.toString= '+event.sender.id.toString());
 		console.log('event.sender.id.toString= '+event.message.attachments[0].payload.url.toString());
 		console.log('enviando URL');
-		/*this.doTextResponse(event.sender.id.toString(),event.message.attachments[0].payload.url.toString());
+		//this.doTextResponse(event.sender.id.toString(),event.message.attachments[0].payload.url.toString());
 			let messageData = {
 				"attachment": {
 					"type": "image",
 					"payload": { url: event.message.attachments[0].payload.url.toString()}
 				}
 			}
-			sendFBMessage (event.sender,messageData);	
-			*/
+			//sendFBMessage (event.sender,messageData);	
 		console.log('return null');
         return null;
     }
