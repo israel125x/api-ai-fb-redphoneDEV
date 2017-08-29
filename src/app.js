1aed07199a91e3ded16b5f2018b7948626c761b3
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-29 12:25:44 -0500

    dcd
    dcdc

--- src/app.js
+++ src/app.js
@@ -381,7 +381,7 @@ class FacebookBot {
 				this.doTextResponse(event.sender.id.toString(),"la ultima repuesta fue :"+consultarID(event.sender.id)+" :) ");
 				}
 				if(event.message.text=="Iniciar encuestas"){
-				iniciarEncuestas(event);
+				//iniciarEncuestas(event);
 				//return ' ';
 				}
 				if(event.message.text=="info"){
