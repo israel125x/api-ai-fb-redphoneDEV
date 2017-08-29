a1fbba31c36e219459e12648d827a6f8f1934e33
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-29 16:31:24 -0500

    dsc
    dcdsc

--- src/app.js
+++ src/app.js
@@ -691,10 +691,10 @@ app.get('/webhook/', (req, res) => {
 app.post('/webhook/', (req, res) => {
     try {
 		//console.log("req = "+JSON.stringify(req));
-        const data = JSONbig.parse(req.body);
-		const head = JSONbig.parse(reqheaders); 
-		console.log("req body = "+JSON.stringify(data));
-		console.log("req reqheaders = "+JSON.stringify(head));
+        //const data = JSONbig.parse(req.body);
+		//const head = JSONbig.parse(reqheaders); 
+		//console.log("req body = "+JSON.stringify(data));
+		//console.log("req reqheaders = "+JSON.stringify(head));
         if (data.entry) {
             let entries = data.entry;
             entries.forEach((entry) => {
