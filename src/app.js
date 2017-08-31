0eaf3ef184596dc58f5a98344a45f991760c0c5d
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-31 12:23:40 -0500

    dfsf
    dfdsfsdf

--- src/app.js
+++ src/app.js
@@ -406,9 +406,9 @@ listarRegistrados(){
 				//this.doTextResponse(event.sender.id.toString(),"la ultima repuesta fue :"+consultarID(event.sender.id)+" :) ");
 				}
 				if(event.message.text=="Xx"){
-					/*var lista=listarRegistrados();
+					var lista=listarRegistrados();
 					console.log("lista.length: ",lista.length);
-					//realiza un segundo intento de obtencion
+					/*realiza un segundo intento de obtencion
 					if(lista.length==0){
 					lista=listarRegistrados();	
 					console.log("lista.length: ",lista.length);
