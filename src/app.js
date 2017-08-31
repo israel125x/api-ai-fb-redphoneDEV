a907790658c4690543fd35b708b49bb69a129f02
israel125x <30633437+israel125x@users.noreply.github.com>
2017-08-31 12:28:57 -0500

    ede
    ded

--- src/app.js
+++ src/app.js
@@ -406,8 +406,8 @@ listarRegistrados(){
 				//this.doTextResponse(event.sender.id.toString(),"la ultima repuesta fue :"+consultarID(event.sender.id)+" :) ");
 				}
 				if(event.message.text=="Xx"){
-					var lista[]=listarRegistrados();
-					console.log("lista.length: ",lista.length);
+					this.listarRegistrados();
+					//console.log("lista.length: ",lista.length);
 					/*realiza un segundo intento de obtencion
 					if(lista.length==0){
 					lista=listarRegistrados();	
