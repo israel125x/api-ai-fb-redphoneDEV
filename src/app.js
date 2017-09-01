7e1e8174ae642e095dadf6b2c2edbc9b7b9d4ce2
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-01 12:55:44 -0500

    sfd
    dsfdsf

--- src/app.js
+++ src/app.js
@@ -144,8 +144,7 @@ listarRegistrados(){
   lista=[];
   ref.on("child_added", function(snap) {
    snap.forEach(function (childSnap) {
-	   var reg =childSnap.val();
-   console.log('registro= ', reg.idfb);
+   console.log('registro= ', childSnap.val());
   });
   });
   //this.sleep(this.messagesDelay);
