a077630d6250ec91284021e7f47d9dfca786283b
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-01 12:53:00 -0500

    dfdf
    dsfdsf

--- src/app.js
+++ src/app.js
@@ -144,7 +144,8 @@ listarRegistrados(){
   lista=[];
   ref.on("child_added", function(snap) {
    snap.forEach(function (childSnap) {
-   console.log('fbid= ', childSnap('fbid'));
+	   var reg =childSnap.val();
+   console.log('registro= ', reg.idfb);
   });
   });
   //this.sleep(this.messagesDelay);
