88e258d0c56a4daf6ad736d261de3d1a485860e9
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-04 16:29:11 -0500

    gdfg
    dfgdfgfd

--- src/app.js
+++ src/app.js
@@ -134,17 +134,16 @@ class FacebookBot {
 //funcion que inicializa las encuestas-consulta id de usuarios registrados en Firebase------------------------ 
 listarRegistrados(){	
 
-function asyncSqrt(value, callback) {
-    console.log('START execution with value =', value);
-    setTimeout(function () {
-        callback(value, value * 1000);
-    }, 0 | Math.random() * 100);
+function asyncSqrt(callback) {
+    console.log('START execution');
+        callback(1000);
+    }
 }
  
-asyncSqrt(0, function (value, result) {
-    console.log('END execution with value =', value, 'and result =', result);
-    asyncSqrt(1, function (value, result) {
-        console.log('END execution with value =', value, 'and result =', result);
+asyncSqrt( function (result) {
+    console.log('END and result =', result);
+    asyncSqrt(function (result) {
+        console.log('END and result =', result+100);
     });
 });
   /*var db = firebase.database();
