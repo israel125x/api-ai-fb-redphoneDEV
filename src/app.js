4aea38189b641e597652205b279dd0fec05bdbf2
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-05 15:20:17 -0500

    bgrgrg
    rgrgerg4t44t

--- src/app.js
+++ src/app.js
@@ -178,6 +178,7 @@ function asyncSqrt2(value, callback) {
 				for (var i = 0; i < value.length; i++) {
 				console.log('lista['+i+']: '+value[i]);	
 				//this.sendFBMessage (value[i],messageData);
+				}
 				callback(null,'OK');
     
 }
