ff306abd78d84a34245953efc62fbde3f687f8d5
israel125x <30633437+israel125x@users.noreply.github.com>
2017-09-12 10:42:31 -0500

    dcd
    dsfdsf

--- src/app.js
+++ src/app.js
@@ -791,9 +791,9 @@ app.post('/webhook/', (req, res) => {
                                 //api ai no esta abilitado para resivir eventos tipo FACEBOOK_LOCATION
 								
 								if (locations.length > 0) {
-									//var latitud= event.message.attachments[0].payload.coordinates.lat;
-									//var longitud= event.message.attachments[0].payload.coordinates.long.toString();
-									geocoder.reverse({lat:45.767, lon:4.833})
+									var latitud= event.message.attachments[0].payload.coordinates.lat;
+									var longitud= event.message.attachments[0].payload.coordinates.long.toString();
+									geocoder.reverse({lat:latitud, lon:longitud})
 									.then(function(res) {
 									console.log('JSON.stringify(res): ',JSON.stringify(res));
 									})
